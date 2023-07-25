8440d3ba4fe5ce5b316298dd243c17bc9165c6ab
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2023-07-25 22:54:02 +0200

    Add `send.rs` example

---
+++ examples/send.rs
@@ -0,0 +1,92 @@
+use std::{collections::VecDeque, error::Error};
+
+use cancellable::{async_trait, Cancellable, CancellationResult, CancellationToken, SenderHandle};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+
+#[derive(Debug)]
+struct NumberSender {
+    inner: UnboundedSender<i32>,
+}
+
+#[async_trait]
+impl SenderHandle for NumberSender {
+    type Item = i32;
+
+    async fn send(&mut self, item: Self::Item) -> Result<(), Self::Item> {
+        self.inner.send(item).map_err(|_| item)?;
+        Ok(())
+    }
+}
+
+struct Multiplier {
+    number_receiver: UnboundedReceiver<i32>,
+    number_sender: Option<UnboundedSender<i32>>,
+}
+
+impl Multiplier {
+    fn new() -> Self {
+        let (sender, receiver) = unbounded_channel();
+
+        Self {
+            number_receiver: receiver,
+            number_sender: Some(sender),
+        }
+    }
+}
+
+#[async_trait]
+impl Cancellable for Multiplier {
+    type Result = i32;
+    type Handle = NumberSender;
+    type Error = anyhow::Error;
+
+    async fn new_handle(&mut self) -> Self::Handle {
+        let sender = self
+            .number_sender
+            .take()
+            .expect("number_sender to be present");
+
+        NumberSender { inner: sender }
+    }
+
+    async fn run(&mut self) -> Result<CancellationResult<Self::Result>, Self::Error> {
+        let number = self
+            .number_receiver
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel has been closed."))?;
+
+        Ok(CancellationResult::item(number * 2))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let cancellation_token = CancellationToken::new();
+    let listener = Multiplier::new();
+
+    let input: VecDeque<_> = [42, 13, 37].into();
+    let mut expected: VecDeque<_> = input.iter().map(|i| i * 2).collect();
+
+    let mut handle = listener
+        .spawn_with_callback(cancellation_token.child_token(), move |number| {
+            let Some(front) = expected.pop_front() else {
+                panic!("Too many inputs");
+            };
+
+            println!("Received {}.", number);
+            assert_eq!(front, number);
+            Ok(())
+        })
+        .await;
+
+    for i in input {
+        println!("Sending {}...", i);
+        assert!(handle.send(i).await.is_ok());
+    }
+
+    cancellation_token.cancel();
+    handle.await??;
+
+    Ok(())
+}
--- src/sender_handle.rs
+++ src/sender_handle.rs
@@ -2,6 +2,70 @@ use async_trait::async_trait;
 
 /// Helper trait that can be used to define a common interface for handles to
 /// send values to their services.
+///
+/// # Examples
+///
+/// ```
+/// use cancellable::{async_trait, Cancellable, CancellationResult, SenderHandle};
+/// use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+///
+/// #[derive(Debug)]
+/// struct NumberSender {
+///     inner: UnboundedSender<i32>,
+/// }
+///
+/// #[async_trait]
+/// impl SenderHandle for NumberSender {
+///     type Item = i32;
+///
+///     async fn send(&mut self, item: Self::Item) -> Result<(), Self::Item> {
+///         self.inner.send(item).map_err(|_| item)?;
+///         Ok(())
+///     }
+/// }
+///
+/// struct Multiplier {
+///     number_receiver: UnboundedReceiver<i32>,
+///     number_sender: Option<UnboundedSender<i32>>,
+/// }
+///
+/// impl Multiplier {
+///     fn new() -> Self {
+///         let (sender, receiver) = unbounded_channel();
+///
+///         Self {
+///             number_receiver: receiver,
+///             number_sender: Some(sender),
+///         }
+///     }
+/// }
+///
+/// #[async_trait]
+/// impl Cancellable for Multiplier {
+///     type Result = i32;
+///     type Handle = NumberSender;
+///     type Error = anyhow::Error;
+///
+///     async fn new_handle(&mut self) -> Self::Handle {
+///         let sender = self
+///             .number_sender
+///             .take()
+///             .expect("number_sender to be present");
+///
+///         NumberSender { inner: sender }
+///     }
+///
+///     async fn run(&mut self) -> Result<CancellationResult<Self::Result>, Self::Error> {
+///         let number = self
+///             .number_receiver
+///             .recv()
+///             .await
+///             .ok_or(anyhow::anyhow!("Channel has been closed."))?;
+///
+///         Ok(CancellationResult::item(number * 2))
+///     }
+/// }
+/// ```
 #[async_trait]
 pub trait SenderHandle {
     /// Type of values send to the service.
